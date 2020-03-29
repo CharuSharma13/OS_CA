fb40868de1540c2277551a7d4504ea12a102924e
CharuSharma13 <61303893+CharuSharma13@users.noreply.github.com>
2020-03-29 21:41:32 +0530

    Update Indianrail.java

--- Indianrail.java
+++ Indianrail.java
@@ -3,3 +3,34 @@ import java.util.Scanner;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+public class Indianrail{
+    static int totalNumberOfPassenger;
+    static int noOfFreeSeatsInTheTrain;
+    static class Station {
+        int passengersAtTheStation;
+        int passengersInTrain;
+        int totalBoarderPassengers;
+        final Lock lock;
+        final Condition passengerSeatedCondition;
+        Station(Lock lock,Condition passengerSeatedCondition) {
+            passengersAtTheStation = 0;
+            totalBoarderPassengers = 0;
+            passengersInTrain = 0;
+            this.lock = lock;
+            this.passengerSeatedCondition = passengerSeatedCondition;
+        }
+    }
+    static class Train extends Thread {
+        Station s;
+        Train(Station s) {
+            this.s = s;
+        }
+        public void run() {
+            try {
+                station_load_train(s,noOfFreeSeatsInTheTrain);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
