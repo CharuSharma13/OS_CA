786882529b37753dbd419b3d04dcfd4b470cecb8
CharuSharma13 <61303893+CharuSharma13@users.noreply.github.com>
2020-03-29 21:42:17 +0530

    Update Indianrail.java

--- Indianrail.java
+++ Indianrail.java
@@ -33,4 +33,54 @@ public class Indianrail{
             }
         }
     }
+     static class  Passenger extends Thread {
+        Station s;
+        Passenger(Station s) {
+            this.s = s;
+        }
+        public void run() {
+            try {
+                station_wait_for_train(s);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    static void station_wait_for_train(Station station) throws InterruptedException {
+        station.lock.lock();
+        station.passengersAtTheStation++;
+        //System.out.println(station.passengersAtTheStation);
+        synchronized (station.lock) {
+            station.lock.wait(1);
+        }
+        station.lock.unlock();
+    }
+    static void station_load_train(Station station,int count) throws InterruptedException {
+        station.lock.lock();
+        while((count >0) && (station.passengersAtTheStation > 0))
+        {
+            station.passengersInTrain++;
+           
+            count--;
+        }
+        synchronized (station.passengerSeatedCondition) {
+            station.passengerSeatedCondition.wait(1);
+        }
+        station.lock.unlock();
+    }
+
+    static void station_on_board(Station station ) {
+        station.lock.lock();
+        while(station.passengersInTrain >0 && station.totalBoarderPassengers < totalNumberOfPassenger)
+        {
+        	
+            station.passengerSeatedCondition.signal();
+            station.passengersInTrain--;
+            System.out.println("|\tPassenger Seated");
+            station.totalBoarderPassengers++;
+            
+        }
+        station.passengerSeatedCondition.signalAll();
+        station.lock.unlock();
+    }
 
