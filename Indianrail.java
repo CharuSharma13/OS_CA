544c4cdc7501739a41595ef55970cf153bf3345f
CharuSharma13 <61303893+CharuSharma13@users.noreply.github.com>
2020-03-29 21:40:14 +0530

    Create Indianrail.java

---
+++ Indianrail.java
@@ -0,0 +1,5 @@
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
